7b033e468b4dc7d355b10f4a61e39de615095a5b
simonbrown123 <sbrown7867@gmail.com>
2025-05-11 15:33:35 +1000

    Add project files.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using WebApplication1.Models;
+
+namespace WebApplication.tests
+{
+    public class UnitTest1
+    {
+        private double valueOne = 10;
+        private double valueTwo = 5;
+        private double knownTotal = 15;
+
+        [Fact]
+        public void Test1()
+        {
+            Calculation calc = new Calculation();
+            var totalValue = calc.AddTwoValues(valueOne, valueTwo);
+            Assert.Equal(totalValue, knownTotal);
+        }
+    }
+}
---
+++ WebApplication1/Models/Class.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Calculator
+    {
+        [Display(Name = "First Value")]
+        public double NumberA { get; set; }
+
+        [Display(Name = "Second Value")]
+        public double NumberB { get; set; }
+
+        [Display(Name = "Total")]
+        public double Total { get; set; }
+    }
+
+    public class Calculation
+    {
+        Calculator calcModel;
+        public Calculation()
+        {
+            calcModel = new Calculator();
+        }
+
+        public double AddTwoValues(double valOne, double valTwo)
+        {
+            return valOne + valTwo;
+        }
+    }
+}
